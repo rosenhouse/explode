15c564c330949c8cb954d537aa6bd358bfd86fde
Gabe Rosenhouse <grosenhouse@pivotal.io>
2015-12-18 10:28:21 -0800

    Forward request to game-server.cfapps.io and return the response
    Signed-off-by: Navyasri Canumalla <ncanumalla@pivotal.io>

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+
+	gameServerHost := "game-server.cfapps.io"
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html><body>Game Over!</body></html>")
+
+		r.RequestURI = ""
+		r.URL.Host = gameServerHost
+		r.URL.Scheme = "http"
+		r.Host = gameServerHost
+
+		log.Printf("got request: %+v\n", r)
+
+		response, err := http.DefaultClient.Do(r)
+		if err != nil {
+			log.Printf("error reaching game server: %s", err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Printf("error reading response bytes from game-server", err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+
+		w.WriteHeader(response.StatusCode)
+		w.Write(bodyBytes)
 	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Println("no port set, defaulting...")
 		port = "8080"
 	}
+	log.Printf("listening on port %s...", port)
 	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
